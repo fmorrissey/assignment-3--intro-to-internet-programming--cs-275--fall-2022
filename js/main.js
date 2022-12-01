386b5eec591e9588f837a68f601330555c54b406
fmorrissey <87839728+fmorrissey@users.noreply.github.com>
2022-12-01 00:50:28 -0500

    Update main.js

---
+++ js/main.js
@@ -0,0 +1,8 @@
+window.onload() =>
+{
+    let body = document.querySelector(body);
+    let script = document.createElement(script);
+        
+        script.setAttribute(scr,json/data.json);
+        body.append(script);
+}
