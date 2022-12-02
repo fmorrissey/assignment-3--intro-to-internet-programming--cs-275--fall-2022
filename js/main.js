b856fe06e612fd27850ccaf7fe182ff33810fa9a
fmorrissey <87839728+fmorrissey@users.noreply.github.com>
2022-12-01 20:51:11 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,60 +1,48 @@
 function AlbumReviews(json)
 {
-    let slideshow = document.querySelectorAll('div') [1];
-    let slide =0;
+    let slideshow = document.querySelector('.carousel-slides') [1];
+    let slide = '';
+    let leftarrow = document.querySelectorAll('a') [0];
+    let rightarrow = document.querySelectorAll('a') [1];
+    let album = document.getElementsByClassName('.carousel-slides') [0];
+    let width = 0;
 
-    for (let i=0; i< 4; i++) {
-        let div = document.createElement('div');
-        div.setAttribute('class' ,'container');
-        div.innerHTML = 
-        `<div class="item">
-          <h3>`+(json[slide].album)+`</h3>
-          <a href="`+(json[slide].url)+`">`+(json[slide].artist)+`</a>
-        </div>
-        <img class="item" src="`+(json[slide].cover_image.path)+`" width="`+(json[slide].cover_image.width)+`"
-        height="`+(json[slide].cover_image.height)+`" alt="`+(json[slide].cover_image.alt_content)+`">
-        <p>`+(json[slide].review.content)+`</p>
-        <p>—<a href="`+(json[slide].review.url)+`">`+(json[slide].review.source)+`</a></p>`;
-        slideshow.append(div);
-        slide += 1;
-    }
-}
+    for (let i=0; i< json.length; i++) {
+        slide += '<div class="every-carousel-slide">';
+        slide += '<p><a></a></p>';
+        slide += '<p><a></a></p>';
+        slide += '<a href="`+(json[i].url)+`">`+(json[slide].artist)`</a>';
+        slide += '<p>$(json[i].album)</p>';
+        slide += <img src= "$json[i].cover_image.path}" alt = "${json[i].cover_image.alt_content}" width = "${json[i].cover_image.width}" height = "${json[i].cover_image.height}">`;
+        slide += '<p>${json[i].review.content}</p>';
+        slide += '<a href="${json[i].review.url}">`-${json[i].review.source}</a>'
+        slide += '</div>';
+     }   
+    slideslow.innerHTML = slide;
 
 window.onload = () => {
-    let leftButton = document.querySelectorAll(`nav > a > img`)[0];
-    let rightButton = document.querySelectorAll(`nav > a > img`)[1]; 
-    leftButton.classList.add(`hidden`); 
-    leftButton.classList.add(`button-L`);
-    rightButton.classList.add(`button-R`);
-    let movement = document.querySelectorAll(`div`)[1];
-    movement.classList.add(`move`); 
     let body = document.querySelector(`body`);
     let script = document.createElement(`script`);
+    let right = document.createElement(`div`);
+    right.classList.add(`right`);
+    let left = document.createElement(`div`);
+    left.classList.add(`left`);
+    let cover = document.getElementsByClassNumber(`carousel`);
+    
     script.setAttribute(`src`,`json/data.json`);
-    body.append(script); 
-    let parent = document.getElementsByClassName(`carousel-slides`)[0];
-    let width = 0;
-    let index = 0;
+    body.append(script);
+    cover.appendChild(right);
+    cover.appendChild(left);
+};
 
-    let checkSlide = () => {
-        if (index > 0 && index < 3) {
-            leftButton.classList.remove(`hidden`);
-            rightButton.classList.remove(`hidden`);
-        } else if (index === 0) {
-            leftButton.classList.add(`hidden`);
-        } else if (index === 3){
-            rightButton.classList.add(`hidden`);
-        }
-    };
-
-    rightButton.addEventListener(`click`, () => {
+    rightarrow.addEventListener(`click`, () => {
         width -= 680;
         parent.style.transform = `translate(`+width+`px)`;
         index += 1;
         checkSlide();
-    });
+    };
 
-    leftButton.addEventListener(`click`, () => {
+    leftarrow.addEventListener(`click`, () => {
         width += 680;
         parent.style.transform = `translate(`+width+`px)`;
         index -= 1;
@@ -62,11 +50,15 @@ window.onload = () => {
     });
 
     document.addEventListener(`keydown`, (e) => {
-        if (e.key === `ArrowRight` && !rightButton.classList.contains(`hidden`)) {
+    {
+        if (e.key === `ArrowRight`) 
+        {
             rightButton.click();
-        } else if (e.key === `ArrowLeft` && !leftButton.classList.contains(`hidden`)) {
+        } 
+        if (e.key === `ArrowLeft`) 
+        {
             leftButton.click();
         }
-    });
+     }});
 };
 
